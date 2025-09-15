6b816c8fee9ea95b27c46b21b7290cef84369efd
Milvelas <231250032@smail.nju.edu.cn>
2025-09-15 20:10:06 +0800

    Lab1 level1

--- MachineLearningLab2025autumn/Lab1/train.py
+++ MachineLearningLab2025autumn/Lab1/train.py
@@ -8,24 +8,34 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# 1. 读取处理好的数据
+# 读取处理好的数据
+print("读取处理好的数据...")
 merged_data = pd.read_excel("Lab1/yii2/processed_features.xlsx")
-merged_data = merged_data.fillna(0)
-# 检查合并后的数据
-print("合并后的数据示例：")
-print(merged_data.head())
 
 # 添加首个反馈时长 (TFR) 和关闭时长 (TTC) 列
 merged_data['created_at'] = pd.to_datetime(merged_data['created_at'])
 merged_data['updated_at'] = pd.to_datetime(merged_data['updated_at'])
 merged_data['closed_at'] = pd.to_datetime(merged_data['closed_at'])
 merged_data['merged_at'] = pd.to_datetime(merged_data['merged_at'])
+merged_data = merged_data.dropna(subset=["closed_at"])
 
 # 计算 TFR 和 TTC
 merged_data['TFR'] = (merged_data['updated_at'] - merged_data['created_at']).dt.total_seconds() / 3600  # 转换为小时
 merged_data['TTC'] = (merged_data[['merged_at', 'closed_at']].min(axis=1) - merged_data['created_at']).dt.total_seconds() / 3600  # 转换为小时
 
 merged_data['created_at'] = pd.to_datetime(merged_data['created_at'])
+
+drop_cols = ["modify_proportion","modify_entropy"]
+merged_data = merged_data.drop(columns=drop_cols, errors="ignore")
+
+ignore_cols = ["TFR", "TTC","created_at","number","updated_at","closed_at","merged_at"]
+cols_to_fill = [c for c in merged_data.columns if c not in ignore_cols]
+
+merged_data[cols_to_fill] = merged_data[cols_to_fill].fillna(0)
+
+# 检查合并后的数据
+print("数据示例：")
+print(merged_data.head())
 
 train_df = merged_data[merged_data["created_at"] < "2021-06-01"]
 test_df  = merged_data[merged_data["created_at"] >= "2021-06-01"]
@@ -46,16 +56,16 @@ X_train = train_df.drop(columns=drop_cols, errors="ignore")
 X_test  = test_df.drop(columns=drop_cols, errors="ignore")
 print("特征维度:", X_train.shape[1])
 
-
-
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test  = scaler.transform(X_test)
 
+
+print("训练模型...")
+
 models = {
     "LinearRegression": LinearRegression(),
     "Ridge": Ridge(alpha=1.0),
-    "RandomForest": RandomForestRegressor(n_estimators=100, random_state=42)
 }
 
 for name, model in models.items():
